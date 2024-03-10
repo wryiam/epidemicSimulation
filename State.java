0cd49f24c2c771b93442f49363b0047838339ed9
William Costales <145481938+wryiam@users.noreply.github.com>
2024-03-10 22:04:05 +0000

    Added Files

---
+++ State.java
@@ -0,0 +1,8 @@
+
+/**
+ * Enumeration class Type - Health states of people during an epidemic simulation
+ */
+public enum State
+{
+    UNVACCINATED, VACCINATED, DOCTOR, VACANTCELL, RECOVERED, DISEASED, ATRISK
+}
